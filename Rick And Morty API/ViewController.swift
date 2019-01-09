40e931c1089098a4b933b7b0a4d113852a26913d
Macinstosh <chhnsn@macbook-air-de-macinstosh.local>
2019-01-09 12:44:07 +0100

    Initial Commit

---
+++ Rick And Morty API/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rick And Morty API
+//
+//  Created by Macinstosh on 09/01/2019.
+//  Copyright © 2019 ozvassilius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
